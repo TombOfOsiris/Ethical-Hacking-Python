e22649ace68a3f11521c76889feaec451a8af4ad
root <timmeyung@gmail.com>
2019-12-06 21:43:08 -0500

    Increase Readability and Reuse 06Dec2019 2143

---
+++ MAC_Address_Changer/mac_changer.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python
+
+# The first byte of the mac address cannot be odd. Add input validation to verify.
+
+import subprocess
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface or not options.new_mac:
+        parser.error("[-] Please specify an interface and mac, use --help for more info")
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
