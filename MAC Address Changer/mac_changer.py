2d07a9a2bba10b9af30217133bdaac2bd80a609d
root <timmeyung@gmail.com>
2019-12-06 21:09:10 -0500

    Handling Command Line Args 06Dec2019 2109

--- MAC Address Changer/mac_changer.py
+++ MAC Address Changer/mac_changer.py
@@ -1,12 +1,22 @@
 #!/user/bin/env python
 
-import subprocess
+# The first byte of the mac address cannot be odd. Add input validation to fix.
 
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC Address for " + interface + " to " + new_mac)
 
-subprocess.run(["ifconfig", interface, "down"])
-subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.run(["ifconfig", interface, "up"])
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
