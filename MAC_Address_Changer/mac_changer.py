4214b876dba7089c09f605f5dbac90f478703310
root <timmeyung@gmail.com>
2019-12-09 11:21:20 -0500

    Implement Validation of MAC Change 09Dec2019 1121

--- MAC_Address_Changer/mac_changer.py
+++ MAC_Address_Changer/mac_changer.py
@@ -18,25 +18,33 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    print("[-] Changing MAC Address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
-def verify_mac_change(interface, new_mac):
-    ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
     if mac_address_search_result:
-        if not ifconfig_result == mac_address_search_result.group(0):
-            print("[-] MAC Address for " + interface + " was unable to be changed. Retaining old MAC address: " +
-                  mac_address_search_result.group(0))
-        else:
-            print("[-] MAC Address for " + interface + " successfully changed to: " + new_mac)
+        return mac_address_search_result.group(0)
     else:
         print("[-] Could not read MAC address.")
 
 
+def verify_mac_change(options, current_mac):
+    if not options.new_mac == current_mac:
+        print("[-] MAC Address for " + options.interface + "was unable to be changed. \n[-] Retaining old MAC "
+                                                           "address: " + current_mac)
+    else:
+        print("[-] MAC Address for " + options.interface + " successfully changed to: " + options.new_mac)
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
-verify_mac_change(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+verify_mac_change(options, current_mac)
+print("Current MAC = " + str(current_mac))
