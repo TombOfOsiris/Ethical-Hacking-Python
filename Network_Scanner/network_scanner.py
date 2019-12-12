febdc2d55694e5c074404f73350193c732903f9c
root <timmeyung@gmail.com>
2019-12-12 07:36:50 -0500

    Start Network Scanner 12Dec2019 0736

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -5,5 +5,8 @@ import scapy.all as scapy
 
 def scan(ip):
     scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
 
-scan("10.0.2.2")
+scan("10.0.2.2/24")
